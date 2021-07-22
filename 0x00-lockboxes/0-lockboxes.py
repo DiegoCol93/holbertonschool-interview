812f2686db2965b2146dc645a68837446159af3a
DiegoCol93 <dfelo93@gmail.com>
2021-07-21 20:58:15 -0500

    |-- FIXED -| Out of range keys.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,64 +1,77 @@
 #!/usr/bin/python3
-""" Module for storing the canUnlockAll() and checkkeysInBox() methods."""
+# """ Module for storing the canUnlockAll() and checkkeysInBox() methods."""
+
+
+# def canUnlockAll(boxes):
+#     """
+#     Determines if all the boxes can be opened.
+#     boxes type : list of list of positive int numbers.
+#     """
+#     # If there only one box, return True as it's open.
+#     if len(boxes) <= 1:
+#         return(True)
+
+#     # Create auxiliary array.
+#     opened = []
+#     # Append False to opened list for each box in boxes.
+#     [opened.append(False) for box in boxes]
+#     # Set box[0] to true as it is already opened.
+#     opened[0] = True
+
+#     # If no keys in 1st box.
+#     if bool(boxes[0]) is False:
+#         return False
+
+#     # Check all keys in the firt box.
+#     for key in boxes[0]:
+#         if key > len(boxes):
+#             continue
+#         if opened[key] is False:
+#             # Call auxiliary recursive method.
+#             opened = checkKeysInBox(key, opened, boxes)
+
+#     if False in opened:
+#         return False
+#     else:
+#         return True
+
+
+# def checkKeysInBox(key, opened, boxes):
+#     """ Checks all boxes reachable with the current key. """
+
+#     # If key out of range.
+#     if key > len(boxes):
+#         return(opened)
+
+#     # If already opened return the opened list.
+#     if opened[key] is True:
+#         return(opened)
+
+#     # Open the box.
+#     opened[key] = True
+
+#     # Look in the box...
+#     # If the next box has something inside.
+#     if bool(boxes[key]):
+
+#         # "For" keys in the box...
+#         index = 0
+#         while index < len(boxes[key]):
+#             # Recursively search.
+#             checkKeysInBox(boxes[key][index], opened, boxes)
+#             index += 1
+
+#     return(opened)
+
+"""Task 0 module"""
 
 
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be opened.
-    boxes type : list of list of positive int numbers.
-    """
-    # If there only one box, return True as it's open.
-    if len(boxes) <= 1:
-        return(True)
-
-    # Create auxiliary array.
-    opened = []
-    # Append False to opened list for each box in boxes.
-    [opened.append(False) for box in boxes]
-    # Set box[0] to true as it is already opened.
-    opened[0] = True
-
-    # If no keys in 1st box.
-    if bool(boxes[0]) is False:
-        return False
-
-    # Check all keys in the firt box.
-    for key in boxes[0]:
-        if key > len(boxes):
-            continue
-        if opened[key] is False:
-            # Call auxiliary recursive method.
-            opened = checkKeysInBox(key, opened, boxes)
-
-    if False in opened:
-        return False
-    else:
-        return True
-
-
-def checkKeysInBox(key, opened, boxes):
-    """ Checks all boxes reachable with the current key. """
-
-    # If key out of range.
-    if key > len(boxes):
-        return(opened)
-
-    # If already opened return the opened list.
-    if opened[key] is True:
-        return(opened)
-
-    # Open the box.
-    opened[key] = True
-
-    # Look in the box...
-    # If the next box has something inside.
-    if bool(boxes[key]):
-
-        # "For" keys in the box...
-        index = 0
-        while index < len(boxes[key]):
-            # Recursively search.
-            checkKeysInBox(boxes[key][index], opened, boxes)
-            index += 1
-
-    return(opened)
+    """Can unlock"""
+    keychain = {0}
+    padlocks = set(range(len(boxes)))
+    for _ in boxes:
+        for padlock in padlocks:
+            if padlock in keychain:
+                keychain.update(boxes[padlock])
+    return padlocks <= keychain
