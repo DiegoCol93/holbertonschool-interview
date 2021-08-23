434a1dfc9afee559494165aa8dd7e4f4c225425c
DiegoCol93 <dfelo93@gmail.com>
2021-08-23 18:50:25 -0500

    |- ADDED -| Source code of solution v0.1

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -6,7 +6,7 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int *array = NULL, size = 0;
+	int *array = NULL, size = 0, i = 0;
 	listint_t *current = NULL;
 
 	current = *head;
@@ -18,8 +18,21 @@ int is_palindrome(listint_t **head)
 			size += 1;
 			current = current->next;
 		}
+		array = malloc(sizeof(int) * size);
+		size = 0;
+		current = *head;
+		while (current)
+		{
+			array[size] = current->n;
+			size += 1;
+			current = current->next;
+		}
+
+		for (i = 0; i < size; i++)
+			if (array[i] != array[size - 1 - i])
+				return (-1);
+
+		return (1);
 	}
-	array = malloc(sizeof(int) * size);
-	printf("%d", size);
-	return (1);
+	return (-1);
 }
