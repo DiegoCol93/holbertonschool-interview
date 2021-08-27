c057ec5921f1e21f909f6494b4a43aca96781f60
DiegoCol93 <dfelo93@gmail.com>
2021-08-26 21:08:32 -0500

    |- ADDED -| Fixed sized 1024 array.

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * is_palindrome - Determines if a linked lis is a palindrome
+ *    @head:     - Pointer to the beggining of the singly linked list
+ *    Return:    - 1 if list is palindrome 0 if not.
+ */
+int is_palindrome(listint_t **head)
+{
+	int array[1024], size = 0, i = 0;
+	listint_t *current = NULL;
+
+	current = *head;
+
+	if (current)
+	{
+		/* while (current) */
+		/* { */
+		/* 	size += 1; */
+		/* 	current = current->next; */
+		/* } */
+		/* array = malloc(sizeof(int) * size); */
+		/* size = 0; */
+		/* current = *head; */
+		while (current)
+		{
+			array[size] = current->n;
+			size += 1;
+			current = current->next;
+		}
+
+		for (i = 0; i < size / 2; i++)
+			if (array[i] != array[size - 1 - i])
+				return (-1);
+	}
+	return (1);
+}
