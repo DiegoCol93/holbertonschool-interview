62210aab0eb98a124980fc5ca8540d5a492bf3be
DiegoCol93 <dfelo93@gmail.com>
2021-08-30 19:55:16 -0500

    |- ADDED -| log parsing project.

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for storing the log parsing method """
+from  sys import stdin
+
+# <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+line_count = 0
+for line in stdin:
+    line_count +=1
+    if 
