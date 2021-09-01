cc2db7da14b11847d3f2373cdeaf219f26778183
DiegoCol93 <dfelo93@gmail.com>
2021-09-01 00:50:46 -0500

    |- ADDED -| SGINT handler.

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -27,31 +27,37 @@ How to use:
 ./0-generator.py | ./0-stats.py
 
 """
-from signal import signal, SIGINT, SIG_IGN, SIGUSR1, pause
+from signal import signal, SIGINT
 from re import search, compile
+from time import sleep
 from sys import stdin
 
-def ignore(sig, frame) -> None:
+
+def ignore(sig, frame, stats):
     """ Captures the SIGINT signal created by Ctrl-C """
-    print("")
-    return
+    print_stats(stats)
+
+def print_stats(stats):
+    """ Prints the stats dicttionary """
+    print(f"File size: {stats['size']}")
+    statuses = ["200", "401", "403", "404", "405", "500"]
+    for status in statuses:
+        print(f"{status}: {stats[status]}")
+
 
 if __name__ == "__main__":
-    stats = {"size": 0,
-             "200": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-    line_count = 0
-    # pause()
-    for line in stdin:
+    stats = {"200": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0,
+             "size": 0}
 
-        RE_pattern = (r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - "
+    line_count = 0
+
+    for line in stdin:
+        RE_pattern = (r"(\d{3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - "
                       r"(\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\]) "
                       r'("GET \/projects\/260 HTTP\/1.1") '
                       r"(\d{3}) (\d+)")
         result = search(RE_pattern, line)
-        if result.groups():
-            ip = result.group(1)
-            date_time = result.group(2)
-            get = result.group(3)
+        if result:
             status = result.group(4)
             file_size = result.group(5)
         else:
@@ -63,11 +69,10 @@ if __name__ == "__main__":
             else:
                 if status == key:
                     stats[key] += 1
-        # Print text. ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-        if (line_count % 10) == 0 and line_count > 1:
-            print(f"File size: {stats['size']}")
-            for key, value in stats.items():
-                if key != "size":
-                    print(f"{key}: {value}")
 
-        line_count +=1
+        # Print text. ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        if (line_count % 10) == 0 and line_count > 1:
+            print_stats(stats)
+
+        signal(SIGINT, lambda signum, frame: ignore(signum, frame, stats))
+        line_count += 1
