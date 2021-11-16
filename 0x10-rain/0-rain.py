f06d301f364eda78a6c9ddd33f776b758a64389c
DiegoCol93 <dfelo93@gmail.com>
2021-11-15 22:42:34 -0500

    |- ADDED -| 0. Rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Rain calculation module. """
+
+def rain(walls):
+    """ Calculates the amount of water units that fit between an int list. """
+    if walls:
+        l_edge = 0
+        r_edge = len(walls)
+
+        for index in range(walls):
+            
+                
+
+
+
+    else:
+        return 0
