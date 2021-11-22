f0a7f9770b9684aff5ae781f9e0679cea67fc5ea
DiegoCol93 <dfelo93@gmail.com>
2021-11-21 22:42:33 -0500

    |- ADDED -| 0x10. Rain.

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -3,15 +3,27 @@
 
 def rain(walls):
     """ Calculates the amount of water units that fit between an int list. """
-    if walls:
-        l_edge = 0
-        r_edge = len(walls)
-
-        for index in range(walls):
-            
-                
-
-
-
-    else:
+    if not walls or not isinstance(walls, list):
         return 0
+
+    l, water = len(walls), 0
+    left, right = [0] * l, [0] * l
+
+    left[0], right[l - 1] = walls[0], walls[l - 1]
+
+    i, j = 1, l - 2
+
+    while True:
+        if i >= l or j <= -1:
+            break
+
+        left[i] = max(left[i - 1], walls[i])
+        right[j] = max(right[j + 1], walls[j])
+
+        i += 1
+        j -= 1
+
+    for i in range(l):
+        water += min(left[i], right[i]) - walls[i]
+
+    return water
