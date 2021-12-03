9cd54b4d426dd2cddb91b572246112597947b893
DiegoCol93 <dfelo93@gmail.com>
2021-12-02 23:30:36 -0500

    |- ADDED -| 0. Advanced Binary Search header file.

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef __FILE___H_
+#define __FILE___H_
+#include <stdio.h>
+
+/* Advanced Binary Search prototypes. */
+int advanced_binary(int *array, size_t size, int value);
+int recurse(int *array, int min, int max, int value);
+void print_array(int *array, int min, int max);
+
+#endif /* __FILE___H_ */
