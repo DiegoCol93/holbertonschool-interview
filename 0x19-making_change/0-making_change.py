6cddadcc61ee65caeb042a50a53ed5a3060f92ee
DiegoCol93 <dfelo93@gmail.com>
2022-02-03 00:43:48 -0500

    |- ADDED -| 2nd attempt for making change.

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ Module for storing the makeChange() method. """
-import ipdb
 
 
 def makeChange(coins, total):
@@ -11,37 +10,29 @@ def makeChange(coins, total):
         coins: List[], of the available denominations.
         total: Int, Amount of change to provide.
     """
-    return(recursivelyMakeChange(coins, total, 0))
-
-
-def recursivelyMakeChange(coins, total, change):
-    """
-    Method for calculating the number of coins needed to provide the
-    indicated amount of change.
-
-    Args:
-        coins:  List[] of the available denominations.
-        total:  Int representing the amount of change to provide.
-        change: Int the number of coins to return.
-    """
-    if coins:
+    if coins and total > 0:
+        change = 0
         coins.sort(reverse=True)
-        times_denominator_fits = total / coins[0]
-        # print(coins)
-        # print(int(times_denominator_fits))
 
-        if (times_denominator_fits).is_integer:
-            change += int(times_denominator_fits)
-            remaining = total - (int(times_denominator_fits) * coins[0])
-            print(change)
-            coins.pop(0)
-            print("\033[92mBEFORE RECURSION\033[m")
-            # print(remaining, int(times_denominator_fits))
-            # change += int(times_denominator_fits)
-            recursivelyMakeChange(coins, remaining, change)
-            # print("\033[92mAFTER RECURSION\033[m")
-            return(change)
-        else:
-            return 0
-    else:
-        return 0
+        for denominator in coins:
+            times_denominator_fits = total / denominator
+            # print(times_denominator_fits)
+
+            if int(times_denominator_fits:)
+
+            if (times_denominator_fits).is_integer and total > 0:
+                change += int(times_denominator_fits)
+                print(change)
+                # print(total)
+
+                if total <= 0:
+                    print("BREAK")
+                    break
+
+                total = total - (
+                    int(times_denominator_fits) * coins[0]
+                )
+
+                change += int(times_denominator_fits)
+
+    return(change)
