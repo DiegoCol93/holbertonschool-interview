7bff5869885119e109d3bc4335eef01b8d8c19d3
DiegoCol93 <dfelo93@gmail.com>
2022-08-04 21:55:19 -0500

    ADDED solution.

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module for storing the pascal_triangle function. """
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle.
+    """
+    matrix = []
+    for i in range(n):
+        actualNumber = [None for _ in range(i + 1)]
+        actualNumber[0], actualNumber[-1] = 1, 1
+        for j in range(1, len(actualNumber) - 1):
+            actualNumber[j] = matrix[i - 1][j - 1] + matrix[i - 1][j]
+        matrix.append(actualNumber)
+    return matrix
